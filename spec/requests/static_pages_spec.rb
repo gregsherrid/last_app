c998ec3f870279bd58c7b2d9ebbc806e17634db9
Gregory Sherrid <greg@kotsea.com>
2013-04-03 02:49:56 -0400

    Dynamically generate titles for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,11 @@ describe "Static pages" do
         visit '/static_pages/home'
         page.should have_content( 'Sample App' )
     end
+
+    it "should have the right title" do
+        visit '/static_pages/home'
+        page.should have_selector('title', :text => " | Home" )
+    end
   end
 
   describe "Help page" do
@@ -18,6 +23,23 @@ describe "Static pages" do
         visit '/static_pages/help'
         page.should have_content( 'Help' )
     end
+
+    it "should have the right title" do
+        visit '/static_pages/help'
+        page.should have_selector('title', :text => " | Help" )
+    end
   end
 
+  describe "About page" do
+    it "should have the content 'Avout us'" do
+
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+
+    it "should have the right title" do
+        visit '/static_pages/about'
+        page.should have_selector('title', :text => " | About Us" )
+    end
+  end
 end
