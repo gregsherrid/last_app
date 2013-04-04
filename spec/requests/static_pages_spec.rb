295088fb2f796f5d9dec051f369d469aa407f55a
Gregory Sherrid <greg@kotsea.com>
2013-04-04 01:12:34 -0400

    Change layout structure and simplifies spec testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,44 +3,49 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    before { visit root_path }
+    it { should have_selector( 'h1', text: 'Sample App' ) }
+    it { should have_selector('title', text: full_title('') ) }
+    it { should_not have_selector('title', :text => "| Home" ) }
 
-        visit '/static_pages/home'
-        page.should have_content( 'Sample App' )
+  end
+
+  describe "Contact page" do
+
+    before { visit contact_path }
+    it "should have the content 'Contact'" do
+        page.should have_selector( 'h1', text: 'Contact' )
     end
 
     it "should have the right title" do
-        visit '/static_pages/home'
-        page.should have_selector('title', :text => base_title )
+        page.should have_selector('title', :text => "#{base_title} | Contact" )
     end
   end
 
+
   describe "Help page" do
 
+    before { visit help_path }
     it "should have the content 'Help'" do
-
-        visit '/static_pages/help'
         page.should have_content( 'Help' )
     end
 
     it "should have the right title" do
-        visit '/static_pages/help'
         page.should have_selector('title', :text => "#{base_title} | Help" )
     end
   end
 
   describe "About page" do
-    it "should have the content 'Avout us'" do
-
-      visit '/static_pages/about'
+    before { visit about_path }
+    it "should have the content 'About us'" do
       page.should have_content('About Us')
     end
 
     it "should have the right title" do
-        visit '/static_pages/about'
         page.should have_selector('title', :text => "#{base_title} | About Us" )
     end
   end
