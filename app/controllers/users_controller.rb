da8b85091722408deaa045032906c70b5bc2fadb
Gregory Sherrid <greg@kotsea.com>
2013-04-10 14:19:28 -0400

    support updating user info and add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
 
+	before_filter :signed_in_user, only: [:edit, :update, :index]
+	before_filter :correct_user, only: [:edit, :update]
+
 	def show
 		@user = User.find( params[:id] )
 	end
 
 	def new
 		@user = User.new
+	end
+
+	def index
+		@users = User.all
 	end
 
 	def create
@@ -18,4 +25,34 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def edit
+		#@user set in correct_user
+	end
+
+	def update
+		#@user set in correct_user
+		if @user.update_attributes(params[:user])
+			flash[:success] = "Profile updated"
+			sign_in @user
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+		def signed_in_user
+			unless signed_in?
+				store_location
+				flash[:notice] = "Please sign in."
+				redirect_to signin_url
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
 end
