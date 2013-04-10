909636e3c90b12e76e1cace2bfdf344f06dc18f5
Gregory Sherrid <greg@kotsea.com>
2013-04-09 22:46:34 -0400

    Finish sign in/sign out implementation

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ LastApp::Application.routes.draw do
 
   resources :users
   match '/signup', to: 'users#new'
+
+  resources :sessions, only: [:new, :create, :destroy ]
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
